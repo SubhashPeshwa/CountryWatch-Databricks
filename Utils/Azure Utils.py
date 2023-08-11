131720d54eae26eaedccfe7ca74e30dacf770e2f
subhash.peshwa@gmail.com <subhash.peshwa@gmail.com>
2023-08-11 15:24:43 +0000

    cleaner folders

--- Utils/Azure Utils.py
+++ Utils/Azure Utils.py
@@ -1,8 +1,4 @@
 # Databricks notebook source
-dbutils.fs.ls("/")
-
-# COMMAND ----------
-
 import json
 import requests
 import os
@@ -48,16 +44,9 @@ class utils:
 # COMMAND ----------
 
 # Function to get secret value from Key-vault backed secret scopes
+# Create secret scope by using cli or by going here - https://adb-3064869328147302.2.azuredatabricks.net/?o=3064869328147302#secrets/createScope
 def get_secret(scope_name, secret_name):      
   return dbutils.secrets.get(scope = scope_name, key = secret_name)
-
-# COMMAND ----------
-
-datagov_api_key = get_secret("kvdevaue-secret-scope","DATA-GOV-API-KEY")
-
-# COMMAND ----------
-
-datagov_api_key
 
 # COMMAND ----------
 
@@ -84,55 +73,74 @@ def mount_storage(mount_path, datalake_name, datalake_key, source_mount):
 
 # COMMAND ----------
 
-required_mounts = {"fusion-muddyboots":"/mnt/fusion-muddyboots","fusion-informed365":"/mnt/fusion-informed365", "fusion-deltalake":"/mnt/fusion-deltalake", "fusion-rms-id-report":"/mnt/fusion-rms-id-report" , "fusion-raw":"/mnt/fusion-raw", "fusion-rms":"/mnt/fusion-rms","fusion-productrecord":"/mnt/fusion-productrecord","fusion-productspec":"/mnt/fusion-productspec","fusion-productrecord-list":"/mnt/fusion-productrecord-list","fusion-productspec-list":"/mnt/fusion-productspec-list","fusion-mdm":"/mnt/fusion-mdm"}
-  
+storage_key = get_secret("kvdevaue-secret-scope","DATADEVAUE")
+
+# COMMAND ----------
+
+mount_storage(mount_path = "/mnt/data",  
+      datalake_name = "datadevaue",
+      datalake_key =  storage_key,
+      source_mount = "data")
+
+# COMMAND ----------
+
+required_mounts = {"data":"/mnt/data", "government":"/mnt/government", "economy":"/mnt/economy", "policy":"/mnt/policy"}
+datalake_name = "datadevaue"
 all_mounts = [x[0] for x in dbutils.fs.mounts()]
 
 for source_mount,mount_path in required_mounts.items():
   if mount_path not in all_mounts:
-    mount_storage(mount_path = mount_path,
-      datalake_name = get_secret("KV-MERCH-OBI-secret-scope","Fusion-storage-account-name"),
-      datalake_key =  get_secret("KV-MERCH-OBI-secret-scope","Fusion-storage-access-key"),
+    mount_storage(mount_path = mount_path,  
+      datalake_name = datalake_name,
+      datalake_key =  get_secret("kvdevaue-secret-scope","DATADEVAUE"),
       source_mount = source_mount)
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ## Delta Table Operations
+
 
 # COMMAND ----------
 
-#sqlContext.sql("DROP TABLE productrecordlinklist")
-#dbutils.fs.rm('/mnt/productrecords-linklist/delta/',recurse=True)
-
-productListTableFilePath = '/mnt/productrecords-linklist/delta/'
-productRMSLinkTableFilePath = '/mnt/productrecords-rmslink/delta/'
-
-productListTableCreate = "CREATE TABLE IF NOT EXISTS productrecordlinklist (recordID int, recordLink string, specType string, code string, barcode string, retailerProductNumber string, recordReceived date, processStatus string) USING DELTA LOCATION '{0}' PARTITIONED BY (recordID)"
-sqlContext.sql(productListTableCreate.format(productListTableFilePath))
-
-productsRMSLinkTableCreate = "CREATE TABLE IF NOT EXISTS productrecordsrmslink (recordID int, recordLink string, barcode string, rmsId string, rmsIdUpdated date, recordReceived date, processStatus string) USING DELTA LOCATION '{0}' PARTITIONED BY (recordID)"
-sqlContext.sql(productsRMSLinkTableCreate.format(productRMSLinkTableFilePath))
-
-productListTableDelete = "DELETE FROM productrecordlinklist"
-sqlContext.sql(productListTableDelete)
-
-custom_schema_str = 'StructType(list([StructField("_ns1",StringType(),True),StructField("entries",ArrayType(StructType(list([StructField("_ns2",StringType(),True),StructField("_type",StringType(),True),StructField("_xsi",StringType(),True),StructField("recordId",LongType(),True),StructField("recordLink",StringType(),True),StructField("code",StringType(),True),StructField("title",StringType(),True)])),True),True),StructField("entryArray",ArrayType(StructType(list([StructField("recordId",LongType(),True),StructField("recordLink",StringType(),True),StructField("code",StringType(),True),StructField("title",StringType(),True)])),True),True),StructField("nextPage",StringType(),True),StructField("totalRecords",LongType(),True)]))'
-
-productListTableInsert = "INSERT INTO productrecordlinklist SELECT recordid AS recordID, recordLink, NULL as specType, code, NULL as barcode, NULL as retailerProductNumber, current_date() as recordReceived, NULL as processStatus FROM (SELECT inline(entries) from tempProdRecordLinkList)"
-
-productsRmsLinkTableInsert = "INSERT INTO productrecordsrmslink SELECT recordid AS recordID, recordLink, NULL as specType, code, NULL as barcode, NULL as rmsId, NULL as rmsIdUpdated, current_date() as recordReceived,  NULL as processStatus FROM (SELECT inline(entries) from tempProdRecordLinkList)"
 
 
-productListTableSelect = "SELECT recordLink from productrecordlinklist WHERE recordID = {0}"
-productListTableUpdate = "UPDATE productrecordlinklist SET barcode = '{0}', retailerProductNumber = '{1}' WHERE recordID in ({2})"
+# COMMAND ----------
 
-productListTableUpdateStatus = "UPDATE productrecordlinklist SET processStatus = '{0}'"
+# MAGIC %md
+# MAGIC ## Hive Table Operations
 
-productRmsLinkTableDelete = "DELETE from productrecordsrmslink WHERE recordID = {0}"
+# COMMAND ----------
 
-custom_schema = eval(custom_schema_str)
-root_tag = "ProductRecordLinkList"
-row_tag = "ProductRecordLinkList"
-file_path = "/FileStore/tables/temp/Product.xml"
-view_name = "tempProdRecordLinkList"
+# #sqlContext.sql("DROP TABLE productrecordlinklist")
+# #dbutils.fs.rm('/mnt/productrecords-linklist/delta/',recurse=True)
+
+# productListTableFilePath = '/mnt/productrecords-linklist/delta/'
+# productRMSLinkTableFilePath = '/mnt/productrecords-rmslink/delta/'
+
+# productListTableCreate = "CREATE TABLE IF NOT EXISTS productrecordlinklist (recordID int, recordLink string, specType string, code string, barcode string, retailerProductNumber string, recordReceived date, processStatus string) USING DELTA LOCATION '{0}' PARTITIONED BY (recordID)"
+# sqlContext.sql(productListTableCreate.format(productListTableFilePath))
+
+# productsRMSLinkTableCreate = "CREATE TABLE IF NOT EXISTS productrecordsrmslink (recordID int, recordLink string, barcode string, rmsId string, rmsIdUpdated date, recordReceived date, processStatus string) USING DELTA LOCATION '{0}' PARTITIONED BY (recordID)"
+# sqlContext.sql(productsRMSLinkTableCreate.format(productRMSLinkTableFilePath))
+
+# productListTableDelete = "DELETE FROM productrecordlinklist"
+# sqlContext.sql(productListTableDelete)
+
+# custom_schema_str = 'StructType(list([StructField("_ns1",StringType(),True),StructField("entries",ArrayType(StructType(list([StructField("_ns2",StringType(),True),StructField("_type",StringType(),True),StructField("_xsi",StringType(),True),StructField("recordId",LongType(),True),StructField("recordLink",StringType(),True),StructField("code",StringType(),True),StructField("title",StringType(),True)])),True),True),StructField("entryArray",ArrayType(StructType(list([StructField("recordId",LongType(),True),StructField("recordLink",StringType(),True),StructField("code",StringType(),True),StructField("title",StringType(),True)])),True),True),StructField("nextPage",StringType(),True),StructField("totalRecords",LongType(),True)]))'
+
+# productListTableInsert = "INSERT INTO productrecordlinklist SELECT recordid AS recordID, recordLink, NULL as specType, code, NULL as barcode, NULL as retailerProductNumber, current_date() as recordReceived, NULL as processStatus FROM (SELECT inline(entries) from tempProdRecordLinkList)"
+
+# productsRmsLinkTableInsert = "INSERT INTO productrecordsrmslink SELECT recordid AS recordID, recordLink, NULL as specType, code, NULL as barcode, NULL as rmsId, NULL as rmsIdUpdated, current_date() as recordReceived,  NULL as processStatus FROM (SELECT inline(entries) from tempProdRecordLinkList)"
+
+
+# productListTableSelect = "SELECT recordLink from productrecordlinklist WHERE recordID = {0}"
+# productListTableUpdate = "UPDATE productrecordlinklist SET barcode = '{0}', retailerProductNumber = '{1}' WHERE recordID in ({2})"
+
+# productListTableUpdateStatus = "UPDATE productrecordlinklist SET processStatus = '{0}'"
+
+# productRmsLinkTableDelete = "DELETE from productrecordsrmslink WHERE recordID = {0}"
+
+# custom_schema = eval(custom_schema_str)
+# root_tag = "ProductRecordLinkList"
+# row_tag = "ProductRecordLinkList"
+# file_path = "/FileStore/tables/temp/Product.xml"
+# view_name = "tempProdRecordLinkList"
